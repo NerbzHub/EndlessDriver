a68c6fe63b7b6053a5a1668c952a9a5e5db85f04
Nathan Nette <nathan.nette@gmail.com>
2018-11-30 12:34:08 +1100

    The billboard script doesn't quite work too well on the quads due to backface culling so I have made them look at -camera.position which has created a really cool effect of them folding in as I go past them. I think this will look really cool during the game.

--- EndlessDriver/Assets/Scripts/Billboard.cs
+++ EndlessDriver/Assets/Scripts/Billboard.cs
@@ -1,19 +1,31 @@
-﻿using System.Collections;
+﻿//------------------------------------------------------------------------------------------
+// Filename:        Billboard.cs
+//
+// Description:     Sets the object this is attached to's transform to be looking at the 
+//                  main camera every frame.
+//
+// Author:          Nathan Nette     
+// Editors:         Nathan Nette
+//------------------------------------------------------------------------------------------
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
+        // Execute billboard function.
+        BillboardObject(gameObject);
+    }
 
+    /// <summary>
+    /// Every frame, set this gameobject's transform to look at the camera.
+    /// </summary>
+    /// <param name="Go">Intended to be this.gameObject.</param>
+    private void BillboardObject(GameObject Go)
+    {
+        Go.transform.LookAt(-Camera.main.transform.position, Vector3.up);
     }
 }
