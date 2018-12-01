a9fdee438bb1e3cd521884396b2a27726b13b7d6
NerbzHub <nathan.nette@gmail.com>
2018-12-01 23:34:18 +1100

    The game currently creates the tile when it hits the end coliider.

---
+++ EndlessDriver/Assets/Scripts/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector3 v3MoveAmount = Vector3.zero;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveTile();
+    }
+
+    private void MoveTile()
+    {
+        gameObject.GetComponent<Rigidbody>().MovePosition(gameObject.transform.position -= v3MoveAmount);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "EndCollider")
+        {
+            Debug.Log("hit");
+            TileManager.CreateTile();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ EndlessDriver/Assets/Scripts/TileManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    
+
+    // Empty gameobjects for positions of the ones in the scene.
+    public GameObject goSpawnTile = null;
+
+    public GameObject[] m_arrTiles = null;
+    // Array of gameobjects for the tiles.
+    public static GameObject[] s_arrTiles = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        s_arrTiles = m_arrTiles;
+    }
+
+    static public void CreateTile()
+    {
+        
+        // instantiate a new tile at the start position.
+        Instantiate(s_arrTiles[Random.Range(0, (s_arrTiles.Length - 1))]);
+    }
+}
